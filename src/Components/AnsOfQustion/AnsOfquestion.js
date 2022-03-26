9011eef402ba431fa445fa8acace31467ee1e32d
Enamulhaque0 <enamulhaq243@gmail.com>
2022-03-26 23:42:32 +0600

    modify

--- src/Components/AnsOfQustion/AnsOfquestion.js
+++ src/Components/AnsOfQustion/AnsOfquestion.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import "./AnsOfquestion.css"
+import React from "react";
+import "./AnsOfquestion.css";
 const AnsOfquestion = () => {
-    return (
-        <div>
-            <h1>Answer</h1>
+  return (
+    <div>
+     <div className="ans">
+     <h1>Answer</h1>
 
-            <p>1.props are read only. State changes can be asychronous</p>
-        </div>
-    );
+<p>1.props are read only. State changes can be asychronous</p>
+<p>
+  2.eact uses a declarative paradigm that makes it easier to reason about
+  your application and aims to be both efficient and flexible.
+</p>
+     </div>
+    </div>
+  );
 };
 
 export default AnsOfquestion;
